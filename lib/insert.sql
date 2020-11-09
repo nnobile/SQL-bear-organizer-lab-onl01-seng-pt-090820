415cb641fe52ffa0f1885e638cc7a446c57dc7a3
Nick Nobile <nickcnobile@gmail.com>
2020-11-08 22:23:04 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 60, "F", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "M", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "F", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 60, "M", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (5, "Melissa", 60, "F", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (6, "Grinch", 20, "M", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (7, "Wendy", 60, "F", "dark chocolate", "chill n' sweet", 1);
+INSERT INTO bears VALUES (8, null, 60, "F", "dark chocolate", "chill n' sweet", 1);
